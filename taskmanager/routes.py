588897691f10400de39ee076bab6f0224792409c
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2023-11-27 00:35:20 +0000

    Update app routes for new pages

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,3 +5,11 @@ from taskmanager.models import Category, Task
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    return render_template("Add_category.html")
