d9f4b9a9e01e3e984d7a3f507bfcef23e4929c19
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2023-11-28 18:41:16 +0000

    add html and logic to add new tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
+  
+
+// modal initialization
+  let modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal);
+
+
+// datepicker initialisation
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {done: "Select"}
+  })
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+});
