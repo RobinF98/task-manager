126663161c329b77f028b647ed8364dec689de03
RobinF98 <135007436+RobinF98@users.noreply.github.com>
2023-12-02 14:36:01 +0000

    fix add_category url

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -19,7 +19,7 @@ def add_category():
         db.session.add(category)
         db.session.commit()
         return redirect(url_for("categories"))
-    return render_template("Add_category.html")
+    return render_template("add_category.html")
 
 @app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
